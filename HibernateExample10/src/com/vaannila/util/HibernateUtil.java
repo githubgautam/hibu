d9fc01840c28a4d4bbc773747b9170f82ecec2e8
padmanabh gautam <pgautam@translations.com>
2016-08-16 21:51:01 +0530

    added output statements to the util.

--- HibernateExample10/src/com/vaannila/util/HibernateUtil.java
+++ HibernateExample10/src/com/vaannila/util/HibernateUtil.java
@@ -7,6 +7,8 @@ public class HibernateUtil {
 	private static final SessionFactory sessionFactory;
 	static {
 		try {
+			System.out.println("build SessionFactory 1...");
+			System.out.println("build SessionFactory 2...");
 			sessionFactory = new AnnotationConfiguration().configure()
 					.buildSessionFactory();
 		} catch (Throwable ex) {
